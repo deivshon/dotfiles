0f3c45333c6a284d1ea4e21e17589d259134e4e9
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-05 17:54:25 +0100

    Add force links flag in setup

--- setup/setup.py
+++ setup/setup.py
@@ -15,12 +15,17 @@ currentUser = os.path.expanduser("~")
 if(currentUser == "/root"):
     sys.exit("Don't run the script as root!")
 
+forceLinks = False
+if(len(sys.argv) > 1 and sys.argv[1] == "-f"):
+    forceLinks = True
+
 for link in linksList:
     linkSource = linksList[link]["source"].replace("$(setupDir)", setupDir)
     linkTarget = linksList[link]["target"].replace("~", currentUser)
     needSudo = linksList[link]["needSudo"]
 
-    command = ["ln", "-si", linkSource, linkTarget]
+    flags = "-sf" if forceLinks else "-si"
+    command = ["ln", flags, linkSource, linkTarget]
 
     print("Linking", linkSource, "to", linkTarget)
     if(needSudo):
