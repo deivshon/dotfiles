287293d133f275671598841d7cb34da6f4a32f2e
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-05 18:19:19 +0100

    Add change notice in cpu usage script

--- scripts/bar/cpuUsage/cpuUsage.sh
+++ scripts/bar/cpuUsage/cpuUsage.sh
@@ -5,6 +5,11 @@
 # Copyright 2014 Andreas Guldstrand <andreas.guldstrand@gmail.com>
 #
 # Licensed under the terms of the GNU GPL v3, or any later version
+#
+
+#
+# Part of the original script at https://github.com/vivien/i3blocks-contrib/blob/master/cpu_usage/cpu_usage
+#
 
 # Get CPU usage
 $ENV{LC_ALL}="en_US"; # if mpstat is not run under en_US locale, things may break, so make sure it is
