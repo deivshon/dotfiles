5585c31353c9a6844fc3f5f8fae327b997abcb53
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-05 18:52:07 +0100

    Add RAM usage script

---
+++ scripts/bar/RAMusage/RAMusage.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+import os
+
+scriptDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.path.insert(1, scriptDir + "/../../scriptingUtils")
+
+import scriptingUtils
+
+memTotal = memAvailable = 0
+meminfoOutput = scriptingUtils.pipeline([["cat", "/proc/meminfo"], ["grep", "-e", "MemTotal", "-e", "MemAvailable"]]).split("\n")[:2]
+meminfoOutput = [int(scriptingUtils.whitelistChars(meminfoOutput[i], "0123456789")) for i in range(0, len(meminfoOutput))]
+memTotal, memAvailable = meminfoOutput[0], meminfoOutput[1]
+
+inUse = (memTotal - memAvailable)
+percentageInUse = (inUse / memTotal) * 100
+print(str(round(inUse / 1000000, 2)), "G / ", str(round(memTotal / 1000000, 2)), "G (" + str(round(percentageInUse, 2)) + "%)", sep="")
