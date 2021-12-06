0cae61041c8f10372534c8f9232f0ae1a95d5daf
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-06 14:11:31 +0100

    Add directory creation mechanism in setup

--- setup/setup.py
+++ setup/setup.py
@@ -5,6 +5,19 @@ import sys
 import subprocess
 import json
 
+# Assumes the path parameter starts with / and is a path to a file
+def makeDirs(pathToFile):
+    dirNames = pathToFile.split("/")
+    dirNames = [dirNames[i] for i in range(0, len(dirNames)) if dirNames[i] != ""]
+    
+    # Loop skips first and last element as the first is empty and the last is the file name
+    # At the end of the loop, the directory where to put the file exists
+    for i in range(1, len(dirNames)):
+        dir = "/" + "/".join(dirNames[0:i])
+        if(not os.path.isdir(dir)):
+            print("Directory", dir, "doesn't exist, creating it")
+            os.mkdir(dir)
+
 setupDir = os.path.dirname(os.path.realpath(__file__))
 
 f = open("links.json")
@@ -23,6 +36,9 @@ for link in linksList:
     linkSource = linksList[link]["source"].replace("$(setupDir)", setupDir)
     linkTarget = linksList[link]["target"].replace("~", currentUser)
     needSudo = linksList[link]["needSudo"]
+
+    # Creates the directory where the target file needs to be in
+    makeDirs(linkTarget)
 
     flags = "-sf" if forceLinks else "-si"
     command = ["ln", flags, linkSource, linkTarget]
