ea8f8931d7a8bb18a065198e2f27d44252f01ec0
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-06 01:43:58 +0100

    Implement basic wifi bar script

--- scripts/bar/wifi/wifi.py
+++ scripts/bar/wifi/wifi.py
@@ -1,3 +1,17 @@
 #!/bin/python3
 
 # Outputs the nf-mdi-wifi nerd font char if the wifi is up and nf-mdi-wifi_off otherwise
+
+import os
+
+wirelessInterface = ""
+
+for interface in os.listdir("/sys/class/net/"):
+    if(interface.startswith("wlp") or interface.startswith("wlan")):
+        stateFile = open("/sys/class/net/" + interface + "/operstate", "r")
+        state = stateFile.read().strip()
+        print(state)
+        if(state == "up"):
+            print("直")
+        else:
+            print("睊")
