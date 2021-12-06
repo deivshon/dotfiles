64671a6ba239f9a9ee38860b12e6fbe7605656fb
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-06 11:38:09 +0100

    Complete wifi state/quality script

--- scripts/bar/wifi/wifi.py
+++ scripts/bar/wifi/wifi.py
@@ -1,17 +1,44 @@
 #!/bin/python3
 
-# Outputs the nf-mdi-wifi nerd font char if the wifi is up and nf-mdi-wifi_off otherwise
+# Outputs the nf-mdi-wifi nerd font char if the wifi is up, followed by the quality of the signal, and nf-mdi-wifi_off if thw wifi is down
 
 import os
+import sys
+import subprocess
+
+scriptDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.path.insert(1, scriptDir + "/../../scriptingUtils")
+
+import scriptingUtils
 
 wirelessInterface = ""
+output = "睊"
+quality =""
 
 for interface in os.listdir("/sys/class/net/"):
     if(interface.startswith("wlp") or interface.startswith("wlan")):
         stateFile = open("/sys/class/net/" + interface + "/operstate", "r")
         state = stateFile.read().strip()
-        print(state)
         if(state == "up"):
-            print("直")
+            output = "直"
+            if(os.path.isfile("/usr/bin/iw")):
+                quality = subprocess.run(["iw", "dev", interface, "link"], capture_output=True)
+                quality = (quality.stdout.decode()).split("\n")
+                
+                for line in quality:
+                    if("dBm" in line):
+                        quality = int(scriptingUtils.whitelistChars(line.strip(),"0123456789-"))
+                        if(quality > -50):
+                            quality = 100
+                        elif(quality < -100):
+                            quality= 0
+                        else:
+                            quality = (quality + 100) * 2
+
+                        break
+        if(state == "down"):
+            print(output)
+        elif(state == "up" and quality != ""):
+            print(output + str(quality) + "%")
         else:
-            print("睊")
+            print(output)
