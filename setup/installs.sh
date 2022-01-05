4d57ff52df7367367575e2726d064e6a46677519
Davide Cioni <davide.cioni.128@gmail.com>
2022-01-05 01:43:06 +0100

    Add POSIX compliant coloured output in install script

--- setup/installs.sh
+++ setup/installs.sh
@@ -1,39 +1,46 @@
 #!/bin/sh
 
+cyan=$(tput setaf 6 bold)
+red=$(tput setaf 1 bold)
+green=$(tput setaf 2 bold)
+normal=$(tput sgr0)
+
 downloads() {
-    echo "Starting downloads"
+    printf "%sStarting downloads%s\n" "${green}" "${normal}"
 
     # Install dwm-flexipatch
     if ! [ -d ~/.config/dwm-flexipatch ]; then
+        printf "%sStarting dwm-flexipatch download%s\n" "${cyan}" "${normal}"
         git clone https://github.com/bakkeby/dwm-flexipatch ~/.config/dwm-flexipatch/
     else
-        echo "dwm-flexipatch: ~/.config/dwm-flexipatch already exists"
+        printf "%sdwm-flexipatch: ~/.config/dwm-flexipatch already exists%s\n" "${red}" "${normal}"
     fi
 
     # Install slstatus
     if ! [ -d ~/.config/slstatus ]; then
+        printf "%sStarting slstatus download%s\n" "${cyan}" "${normal}"
         git clone https://github.com/drkhsh/slstatus ~/.config/slstatus/
     else
-        echo "slstatus: ~/.config/slstatus already exists"
+        printf "%sslstatus: ~/.config/slstatus already exists%s\n" "${red}" "${normal}"
     fi    
 
-    echo "Downloads over, exiting"
+    printf "%sDownloads over, exiting%s\n" "${green}" "${normal}"
 }
 
 compilations() {
-    echo "Starting compilations"
+    printf "Starting compilations%s\n" "${green}" "${normal}"
 
     # dwm-flexipatch compilation
     cd ~/.config/dwm-flexipatch || exit
-    echo "Starting dwm-flexipatch compilation"
+    printf "%sStarting dwm-flexipatch compilation%s\n" "${cyan}" "${normal}"
     sudo make clean install
 
     # slstatus compilation
     cd ~/.config/slstatus || exit
-    echo "Starting slstatus compilation"
+    printf "%sStarting slstatus compilation%s\n" "${cyan}" "${normal}"
     sudo make clean install
 
-    echo "Compilations over, exiting"
+    printf "%sCompilations over, exiting%s\n" "${green}" "${normal}"
 }
 
 if [ "$1" = "-d" ]; then
