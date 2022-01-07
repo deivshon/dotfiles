af90adeac14160690630bb7f59253e2971826ce4
Davide Cioni <davide.cioni.128@gmail.com>
2022-01-08 00:34:15 +0100

    Add alternative RAM usage POSIX compliant shell script

---
+++ scripts/bar/RAMusage/RAMusage.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+memInfo=$(grep -e MemTotal -e MemAvailable < /proc/meminfo)
+
+while IFS= read -r line; do
+    if [ "$(printf "%s" "$line" | grep "MemTotal")" != "" ]; then
+        memTot=$(printf "%s" "$line" | awk '{print $2}')
+    else
+        memAvail=$(printf "%s" "$line" | awk '{print $2}')
+    fi
+done << EOF
+$memInfo
+EOF
+
+memPerc=$(echo "100 - ($memAvail / $memTot)*100" | bc -l)
+
+memTot=$(echo "$memTot" / 1000000 | bc -l)
+memAvail=$(echo "$memAvail" / 1000000 | bc -l)
+
+memInUse=$(echo "$memTot - $memAvail" | bc -l)
+printf "%.2fG/%.2fG (%.1f%%)" "$memInUse" "$memTot" "$memPerc"
