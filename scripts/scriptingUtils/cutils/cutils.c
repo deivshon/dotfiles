c17672dd628e1240dc00e1e18967e17056bfc5d8
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-09-28 18:48:51 +0200

    Add get command output and get separator C utility functions

--- scripts/scriptingUtils/cutils/cutils.c
+++ scripts/scriptingUtils/cutils/cutils.c
@@ -1,1 +1,46 @@
 #include "cutils.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void get_cmd_output(char *dest, int dest_size, char *cmd, char **cmd_args) {
+    int piped[2];
+    if(pipe(piped) == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    if(fork() == 0) {
+        close(piped[0]);    // close read end of child
+        dup2(piped[1], 1);  // stdout to pipe
+        dup2(piped[1], 2);  // stderr to pipe
+        close(piped[1]);    // close write end of child: no longer needed
+
+        execvp(cmd, cmd_args);
+    }
+    else {
+        strcpy(dest, "");
+
+        int c = 0; // Bytes read into buffer each time
+        char buf[1024] = "";
+        close(piped[1]);    // close write end of parent
+
+        while(c = read(piped[0], buf, sizeof(buf) - 1)) {
+            // Sets the byte after the last one read to '\0', terminating the string
+            buf[c] = '\0';
+
+            if(strlen(dest) + strlen(buf) >= dest_size)
+                break;
+
+            strcat(dest, buf);
+        }
+        close(piped[0]);    // close read end of parent: no longer needed
+    }
+}
+
+void get_separator(char **dest, int argc, char **argv) {
+        for(int i = 0; i < argc; i++) {
+        if(streq("--separator", argv[i])) {
+            (*dest) = SEPARATOR;
+        }
+    }
+}
--- scripts/scriptingUtils/cutils/cutils.h
+++ scripts/scriptingUtils/cutils/cutils.h
@@ -6,8 +6,13 @@
 #define MAX_PATH_LENGTH 4096
 #define NET_INTERFACES_PATH "/sys/class/net"
 
+#define SEPARATOR "|"
+
 #define strip(str) str[strcspn(str, "\n")] = '\0';
 #define streq(str1, str2) !strcmp(str1, str2)
 #define starts_with(str, prefix) !strncmp(str, prefix, strlen(prefix))
+
+void get_separator(char **dest, int argc, char **argv);
+void get_cmd_output(char *dest, int dest_size, char *cmd, char **cmd_args);
 
 #endif
