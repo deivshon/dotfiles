1a2e67f9518138e2e1c0193ea2555f12291c5922
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-08-14 17:47:17 +0200

    fix: :bug: fix battery level not updating in qtile graph widget

--- dots/.config/qtile/battery_graph.py
+++ dots/.config/qtile/battery_graph.py
@@ -1,34 +1,29 @@
 from libqtile.widget import base
 from libqtile.widget.graph import _Graph
 
+
 class BatteryGraph(_Graph):
     """Display a battery level graph"""
 
     orientations = base.ORIENTATION_HORIZONTAL
     fixed_upper_bound = True
 
-
     def __init__(self, battery_path: str, **config):
         _Graph.__init__(self, **config)
         self.battery_path = battery_path
         self.maxvalue = 100
 
-        val = self._getvalues()
-        battery_level = val["level"]
-        self.fulfill(battery_level)
+        battery_charge = self._getvalues()
+        self.fulfill(battery_charge)
 
     def _getvalues(self):
-        val = {}
-
         try:
             with open(f"{self.battery_path}/capacity") as f:
-                val["level"] = int(f.read().strip())
+                level = int(f.read().strip())
         except Exception:
-            val["level"] = 0
+            level = 0
 
-        return val
+        return level
 
     def update_graph(self):
-        val = self._getvalues()
-
-        self.push(val)
+        self.push(self._getvalues())
