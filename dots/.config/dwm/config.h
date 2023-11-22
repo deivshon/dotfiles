9b76a0ad877a33189f41f7d0a2116b5115076ba4
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-11-22 22:10:27 +0100

    fix: :ambulance: fix jetbrainsmono nerd font name in configs
    reason: https://github.com/ryanoasis/nerd-fonts/issues/1434

--- dots/.config/dwm/config.h
+++ dots/.config/dwm/config.h
@@ -61,8 +61,8 @@ static const int showsystray             = 1;   /* 0 means no systray */
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "JetBrainsMonoNerdFont:pixelsize=14" };
-static const char dmenufont[]            = "JetBrainsMonoNerdFont:pixelsize=14";
+static const char *fonts[]               = { "JetBrainsMonoNF:pixelsize=14" };
+static const char dmenufont[]            = "JetBrainsMonoNF:pixelsize=14";
 
 static char c000000[]                    = "#000000"; // placeholder value
 
