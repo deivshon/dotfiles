d90a83c2afeba498b51379886cf910542c5a60ed
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-11-26 13:41:45 +0100

    perf: :zap: improve installed vs code extensions list retrieval

--- setup/lib/dots/appliers/vscode/extensions.py
+++ setup/lib/dots/appliers/vscode/extensions.py
@@ -2,17 +2,52 @@ import os
 import json
 import subprocess
 
+from dataclasses import dataclass
 from typing import Dict, Optional, List
 
+from setup.lib import utils
 from setup.lib import log, LIB_DIR
 from setup.lib.utils import process
 from setup.lib.dots.appliers.applier import Applier
 
 __VSCODE_EXTENSIONS_FILE = os.path.join(
     LIB_DIR, "..", "data", "vscode_extensions.json")
+__VSCODE_INSTALLED_EXTENSIONS_FILE = os.path.join(
+    utils.HOME_DIR, ".vscode", "extensions", "extensions.json")
+__VSCODE_EXTENSION_IDENTIFIER_LABEL = "identifier"
+
+
+@dataclass
+class _ExtensionIdentificationData():
+    id: str
+    uuid: str
 
 
 def __get_installed_extensions() -> Optional[List[str]]:
+    try:
+        installed_extensions: List[str] = []
+        if os.path.isfile(__VSCODE_INSTALLED_EXTENSIONS_FILE):
+            with open(__VSCODE_INSTALLED_EXTENSIONS_FILE) as f:
+                installed_extensions_data = json.loads(f.read())
+
+            for raw_extension_data in installed_extensions_data:
+                if __VSCODE_EXTENSION_IDENTIFIER_LABEL not in raw_extension_data:
+                    continue
+
+                extension_identifier = raw_extension_data[__VSCODE_EXTENSION_IDENTIFIER_LABEL]
+                extension_data = _ExtensionIdentificationData(
+                    **extension_identifier)
+                installed_extensions.append(extension_data.id)
+
+            return installed_extensions
+        else:
+            log.warning(
+                f"Could not get current vscode extensions from file: {log.RED}{__VSCODE_INSTALLED_EXTENSIONS_FILE} does not exist")
+    except Exception as e:
+        log.error(
+            f"Could not get current vscode extensions from file: {log.RED}{e}")
+        log.error(f"Falling back `code --list-extensions` command")
+
     try:
         p = subprocess.run(["code", "--list-extensions"], capture_output=True)
     except Exception as e:
@@ -30,10 +65,13 @@ def __extensions_applier(_: Dict) -> None:
 
     with open(__VSCODE_EXTENSIONS_FILE, "r") as f:
         config_extensions: List[str] = json.loads(f.read())
+    config_extensions = list(map(lambda e: e.lower(), config_extensions))
 
     installed_extensions = __get_installed_extensions()
     if installed_extensions is None:
         return
+
+    installed_extensions = list(map(lambda e: e.lower(), installed_extensions))
 
     needing_install = list(filter(
         lambda e: e not in installed_extensions, config_extensions))
