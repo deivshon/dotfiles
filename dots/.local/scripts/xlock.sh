1640a7d35b82f96d5a036317b4e803ad6958a6c4
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-12-08 14:14:50 +0100

    refactor: :recycle: update lock screen configurations

--- dots/.local/scripts/xlock.sh
+++ dots/.local/scripts/xlock.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
 i3lock \
-    --ring-width=15 \
-    --clock \
-    --time-str=%R \
-    --date-str="%A %e of %B" \
-    --radius=250 \
-    --blur=1 \
+    --ring-width=10 \
+    --radius=240 \
     --indicator \
+    --color=<sub<i3l--inside-color>> \
     --inside-color=<sub<i3l--inside-color>> \
-    --insidever-color=<sub<i3l--inside-ver-color>> \
-    --insidewrong-color=<sub<i3l--inside-wrong-color>> \
+    --insidever-color=<sub<i3l--inside-color>> \
+    --insidewrong-color=<sub<i3l--inside-color>> \
     --keyhl-color=<sub<i3l--key-hl-color>> \
     --bshl-color=<sub<i3l--bs-hl-color>> \
     --separator-color=<sub<i3l--separator-color>> \
-    --line-color=<sub<i3l--line-color>> \
     --line-uses-ring \
     --time-color=<sub<i3l--text-color>> \
     --date-color=<sub<i3l--text-color>> \
@@ -22,8 +18,25 @@ i3lock \
     --modif-color=<sub<i3l--text-color>> \
     --wrong-color=<sub<i3l--text-wrong-color>> \
     --verif-color=<sub<i3l--text-ver-color>> \
-    --date-size=30 \
-    --time-size=60 \
     --ring-color=<sub<i3l--ring-color>> \
     --ringver-color=<sub<i3l--ring-ver-color>> \
-    --ringwrong-color=<sub<i3l--ring-wrong-color>>
+    --ringwrong-color=<sub<i3l--ring-wrong-color>> \
+    --time-font="JetBrainsMono NF" \
+    --date-font="JetBrainsMono NF" \
+    --layout-font="JetBrainsMono NF" \
+    --verif-font="JetBrainsMono NF" \
+    --wrong-font="JetBrainsMono NF" \
+    --greeter-font="JetBrainsMono NF" \
+    --time-size=50 \
+    --date-size=50 \
+    --layout-size=50 \
+    --verif-size=50 \
+    --wrong-size=50 \
+    --greeter-size=50 \
+    --verif-text="Verifying..." \
+    --wrong-text="Wrong!" \
+    --noinput-text="No input" \
+    --lock-text="Locking..." \
+    --lockfailed-text="Lock failed!" \
+    --no-modkey-text \
+    -f
